b6a5e6ff580efd5421fed2e17d97b62f51797458
Viktor Maksymiv <viktor.maksymiv.23@pnu.edu.ua>
2026-01-08 18:36:30 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,69 @@
+import copy
+
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+@pytest.fixture()
+def user_list() -> list:
+    return [
+  {
+    "first_name": "Jack",
+    "last_name": "Holy",
+    "full_name": "Jack Holy",
+  },
+  {
+    "first_name": "Mike",
+    "last_name": "Adams",
+    "full_name": "Mike Adams",
+  },
+]
+
+def test_restore_names_when_list_is_correct(user_list) -> None:
+    user_list_before_editing = copy.deepcopy(user_list)
+
+    restore_names(user_list)
+
+    assert user_list_before_editing == user_list
+
+
+def test_restore_names_when_list_is_empty(user_list) -> None:
+    user_list_before_editing = []
+
+    empty_user_list = []
+
+    restore_names(empty_user_list)
+
+    assert user_list_before_editing == empty_user_list
+
+
+def test_restore_names_when_first_name_is_none(user_list) -> None:
+    user_list_before_editing = copy.deepcopy(user_list)
+
+    user_list[0]["first_name"] = None
+
+    restore_names(user_list)
+
+    assert user_list == user_list_before_editing
+
+
+def test_restore_names_when_there_is_not_first_name_attribute(user_list) -> None:
+    user_list_before_editing = copy.deepcopy(user_list)
+
+    del user_list[0]["first_name"]
+
+    restore_names(user_list)
+
+    assert user_list == user_list_before_editing
+
+def test_restore_names_when_there_are_few_names(user_list) -> None:
+    user_list[0]["full_name"] = "Jack Grok Holy"
+
+    user_list_before_editing = copy.deepcopy(user_list)
+
+    del user_list[0]["first_name"]
+
+    restore_names(user_list)
+
+    assert user_list == user_list_before_editing
+
